37a0ef4e415487a8d2e874c7e3f74bc545bf747a
Edwin Wisse <edwin.wisse@logius.nl>
2023-01-05 12:22:43 +0100

    Update config.js
    Links naar https://gitdocumentatie.logius.nl/publicatie/

--- js/config.js
+++ js/config.js
@@ -99,10 +99,10 @@ var respecConfig = {  // this template doesn''t use all possible config paramete
   nl_organisationPrefix: "LS-",
   nl_markdownTableClass: "dkkvs",
   nl_markdownEmbedImageInFigure: true,
-  nl_organisationStylesURL: "https://publicatie.centrumvoorstandaarden.nl/respec/style/",
+  nl_organisationStylesURL: "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
   nl_organisationPublishURL: "https://gitdocumentatie.logius.nl/publicatie/",
   nl_logo: {
-    src: "https://publicatie.centrumvoorstandaarden.nl/respec/style/logos/figure-logius.svg",
+    src: "https://gitdocumentatie.logius.nl/publicatie/respec/style/logos/figure-logius.svg",
     //src: "http://localhost:8081/respec/style/logos/figure-logius.svg",
     alt: "Logius",
     id: "Logius",
