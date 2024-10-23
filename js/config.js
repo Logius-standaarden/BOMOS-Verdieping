ac781e1a313804a209c751bbd76d1058c02223c1
Alexander Green <alexander.green@logius.nl>
2024-10-23 16:08:58 +0200

    Publicatie uit vertalingbranch

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   alternateFormats: [ { 
         "label" : "pdf",
-        "uri" : "BOMOS-Verdieping.pdf"
+        "uri" : "BOMOS-Substantiation.pdf"
       } ],
   authors: [ { 
         "company" : "HAN University of Applied Sciences",
@@ -27,13 +27,12 @@ var respecConfig = {
     ],
   github: "https://github.com/Logius-standaarden/BOMOS-Verdieping",
   maxTocLevel: 3,
-  nl_markdownTableClass: "dkkvs",
   previousMaturity: "DEF",
   previousPublishVersion: "3.0.0",
   pubDomain: "bomos",
   publishDate: "2023-11-02",
   publishVersion: "3.0.1",
-  shortName: "verdieping",
+  shortName: "verdieping/en",
   specStatus: "DEF",
   specType: "HR",
 
